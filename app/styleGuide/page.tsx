cec7683ed93a6557c3869c7728c99ddb7145102c
Precious Ogwezhi <pogwezhi@gmail.com>
2025-11-16 21:21:11 +0000

    style

---
+++ app/styleGuide/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "@/components/Footer";
+import Button from "@/components/Button";
+
+const page = () => {
+  return (
+    <div>
+      <section>
+        <p>This is a test page</p>
+        <Button size="small" withArrow>Click me</Button>
+      </section>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default page;
---
+++ components/Button.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import clsx from "clsx";
+import ArrowIcon from "@/public/assets/icons/arrow-right.svg";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "outline" | "ghost" | "rounded" | "solo";
+  size?: "small" | "medium" | "large";
+  withArrow?: boolean;
+  arrowColor?: string;
+}
+const Button = ({
+  children,
+  variant = "primary",
+  size = "medium",
+  disabled = false,
+  withArrow = false,
+  className,
+  ...props
+}: ButtonProps) => {
+  const arrowColorClass = clsx({
+    // Primary
+    "text-white ": variant === "primary" && !disabled,
+    "text-white/50": variant === "primary" && disabled,
+    // Outline
+    "text-primary hover:text-primaryDark active:text-primaryDark":
+      variant === "outline" && !disabled,
+    "text-primary-light": variant === "outline" && disabled,
+    // Ghost
+    "text-primary": variant === "ghost" && !disabled,
+    "text-primary-extraLight": variant === "ghost" && disabled,
+  });
+
+  const variantClasses = clsx({
+    "bg-primary text-white hover:bg-primary-dark active:bg-primary-dark rounded-md":
+      variant === "primary" && !disabled,
+    "bg-primary-extralight text-white rounded-md cursor-not-allowed":
+      variant === "primary" && disabled,
+
+    // outline
+
+    "border-[1.5px] text-primary bg-transparent border-primary hover:border-primary-dark active:border-primary-dark  rounded-md":
+      variant === "outline" && !disabled,
+    "border-[1.5px] text-white bg-transparent border-primary-light text-primary-light  rounded-md cursor-not-allowed":
+      variant === "outline" && disabled,
+
+    "bg-transparent text-primary hover:text-primary-dark active:text-primary-dark":
+      variant === "ghost" && !disabled,
+    "bg-transparent text-primary-extraLight cursor-not-allowed":
+      variant === "ghost" && disabled,
+
+    //rounded
+    "bg-primary h-[65.22px] w-[65.22px] text-white hover:bg-primary-dark active:bg-primary-dark rounded-[50%]":
+      variant === "rounded" && !disabled,
+    "bg-primary-extralight h-[65.22px] w-[65.22px] text-white rounded-[50%] cursor-not-allowed":
+      variant === "rounded" && disabled,
+
+    // solo
+    "bg-primary h-[56px] w-[56px] text-white hover:bg-primary-dark active:bg-primary-dark rounded-[50%]":
+      variant === "rounded" && !disabled,
+  });
+
+  const sizeClasses = clsx({
+    "width-[88px] height-[36px] text-base": size === "small",
+    "width-[88px] height-[48px] text-base": size === "medium",
+    "width-[90px] height-[56px] text-base": size === "large",
+  });
+  return (
+    <button
+      className={clsx(
+        sizeClasses,
+        variantClasses,
+        "flex items-center justify-center gap-2",
+        className
+      )}
+      disabled={disabled}
+      {...props}
+    >
+      {withArrow && <ArrowIcon className={clsx("w-6 h-6", arrowColorClass)} />}
+      <span>{children}</span>
+    </button>
+  );
+};
+
+export default Button;
