65d6a528f54b5dd041903d962692066602d8c3cf
Ridwan <lawal6382@gmail.com>
2025-11-17 14:09:12 +0100

    feat(how it works): add animations to the how it works page

--- components/layout/PerkSection.tsx
+++ components/layout/PerkSection.tsx
@@ -20,17 +20,17 @@ const perks = [
 
 export default function PerksSection() {
   return (
-    <section className="w-full flex justify-center py-16">
-      <div className="flex flex-col items-center gap-8 w-[408px] lg:w-[1240px]">
-        <div className="flex flex-col items-center gap-2 lg:gap-4 w-full lg:w-[804px]">
-          <h2 className="text-4xl lg:text-4xl font-bold text-center">
+    <section className="flex w-full justify-center py-16">
+      <div className="flex w-[408px] flex-col items-center gap-8 lg:w-[1240px]">
+        <div className="flex w-full flex-col items-center gap-2 lg:w-[804px] lg:gap-4">
+          <h2 className="text-center text-4xl font-bold lg:text-4xl">
             Perks For Joining Early, Mama!
           </h2>
-          <p className="text-body regular lg:text-2xl text-center lg:w-[552px]">
+          <p className="text-body regular text-center lg:w-[552px] lg:text-2xl">
             Little goodies while you wait for Nora&apos;s big arrive
           </p>
         </div>
-        <div className="grid grid-cols-1 gap-2 lg:grid-cols-3 lg:gap-8 w-full lg:w-full">
+        <div className="grid w-full grid-cols-1 gap-2 lg:w-full lg:grid-cols-3 lg:gap-8">
           {perks.map((perk, index) => (
             <PerkCard
               key={index}
