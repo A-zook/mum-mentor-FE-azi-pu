2a500a2d8122064a83040b54a72a55600425a97c
Lisan al Gaib <chidiobinna0001@gmail.com>
2025-11-24 16:27:16 +0100

    Update APK link to new Appetize.io app instance

--- data/constants.ts
+++ data/constants.ts
@@ -1,3 +1,3 @@
-export const APK_LINK = "https://appetize.io/app/x67s5kzgymwcv2hysynmzvaox4";
+export const APK_LINK = "https://appetize.io/app/b_luhmfjuuji66ctac2n444begke";
 export const VIDEO_URL =
   "https://res.cloudinary.com/debgkcg8v/video/upload/v1763653490/Clear_Explainer_Final_Video_rm358l.mp4";
